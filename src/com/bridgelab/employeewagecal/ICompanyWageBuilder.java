41a334ceb22bdc8c1e5d67c2ec09c35a69e652e3
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-06-26 16:39:56 +0530

    Employee wage cal day 10

---
+++ src/com/bridgelab/employeewagecal/ICompanyWageBuilder.java
@@ -0,0 +1,15 @@
+package com.bridgelab.employeewagecal;
+
+public interface ICompanyWageBuilder {
+
+	public void addCompany(String companyName,
+			int maxWorkingDay, int maxWorkingHour,
+			int wagePerHour);
+
+	public void computeEmpWage();
+	
+	public int totalCompanies();
+	
+	public CompanyWage getTotalEmpWage(String companyName);
+	
+}
