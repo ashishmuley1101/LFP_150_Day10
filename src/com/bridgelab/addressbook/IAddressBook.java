a56ce281d577868d08b9b1d6bb748d9470d387a1
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-06-26 16:09:33 +0530

    Address Book day 10

---
+++ src/com/bridgelab/addressbook/IAddressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelab.addressbook;
+
+public interface IAddressBook {
+	
+		public void addContact();
+		public void showaddress();
+		public void deleteContact();
+		public void updateContact();
+	}
+
